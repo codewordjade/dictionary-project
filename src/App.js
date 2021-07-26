e678c9f7fe5b2e12549223f9b10ae6ac20617ae7
codewordjade <77465075+codewordjade@users.noreply.github.com>
2021-07-26 19:51:21 +0100

    Added header text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
+
         <main>
           <Dictionary />
         </main>
